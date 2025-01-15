18a3be8b3c83d9014e8ec3b542662118720dc671
oilprojects <oscarinfanzong00@gmail.com>
2025-01-15 15:59:04 -0500

    Updated

--- CollaborativeStoryBuilder/app/src/main/java/com/infanzon/collaborativestorybuilder/MainApp/MainActivity.java
+++ CollaborativeStoryBuilder/app/src/main/java/com/infanzon/collaborativestorybuilder/MainApp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.infanzon.collaborativestorybuilder.MainApp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,19 +9,22 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.infanzon.collaborativestorybuilder.R;
+import com.infanzon.collaborativestorybuilder.StoryHandler.CollaborativeStory;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        FloatingActionButton fabAdd = findViewById(R.id.fabAdd);
+
+        fabAdd.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, CollaborativeStory.class);
+            startActivity(intent);
         });
     }
 }
---
+++ CollaborativeStoryBuilder/app/src/main/java/com/infanzon/collaborativestorybuilder/StoryHandler/CollaborativeStory.java
@@ -0,0 +1,16 @@
+package com.infanzon.collaborativestorybuilder.StoryHandler;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.infanzon.collaborativestorybuilder.R;
+
+public class CollaborativeStory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_collaborative_story);
+    }
+}
